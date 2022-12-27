9f3cada65d550425b037f7f9f73f447fec47feac
Enrique <octepc.97@gmail.com>
2022-12-27 18:04:14 -0500

    rendered products to the layout

--- store-app/src/app/app.component.ts
+++ store-app/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Product } from "./models/product.model";
 
 @Component({
   selector: 'app-root',
@@ -10,5 +11,40 @@ export class AppComponent {
 
   onLoaded(img: string){
     console.log('log padre', img)
+  }
+
+  products: Product[]=[ {
+    id: '1',
+    name: 'EL mejor juguete',
+    price: 565,
+    image: './assets/images/toy.jpg',
+    description: 'an amazing toy to your kiddo!'
+  },
+  {
+    id: '2',
+    name: 'Bicicleta casi nueva',
+    price: 356,
+    image: './assets/images/bike.jpg'
+  },
+  {
+    id: '3',
+    name: 'Colleción de albumnes',
+    price: 34,
+    image: './assets/images/album.jpg'
+  },
+  {
+    id: '4',
+    name: 'Mis libros',
+    price: 23,
+    image: './assets/images/books.jpg'
+  }
+
+  ]
+
+  product:Product={
+    id:'1',
+    name: 'Producto 1',
+    image:  'https://picsum.photos/800',
+    price: 10,
   }
 }
